3fbf23a4bafacf2f6677d1ab0f1cfe74ae598130
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-04-23 16:07:06 +0530

    feat: playground v1

--- playground/src/app/page.tsx
+++ playground/src/app/page.tsx
@@ -1,26 +1,57 @@
 "use client";
-import { Ignition } from "ignition-js-sdk";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
 
 export default function Home() {
 
-	const i = new Ignition({
-		key:"abc123",
-		url:"ws://ignition-shared-v3.onrender.com"
-	})
+	const [socket, setSocket] = useState<any>(null);
+	const [syncState, setSyncState] = useState("");
+	const [tempState, setTempState] = useState("");	
 
 	useEffect(() => {
-		i.subscribe("test")
-		i.on("test", (data) => {
-			console.log(data)
-		})
-	}, [])
 
-	const [syncState, setSyncState] = useState("");
+		const socket = io("https://ignition-shared-v3.onrender.com", {
+			auth:{
+				token:"abc123"
+			}
+		});
+
+		setSocket(socket);
+
+		socket.on("connect", () => {
+			console.log("connected");
+			socket.emit("JOIN", { key:"abc123", group_id:"test" }, (data:any) => {
+				console.log(data)
+			})
+			socket.on("sync", (data:any) => {
+				// alert(data)
+				console.log(data)
+				setTempState(data)
+			})
+		})
+
+	}, []);
+
+	useEffect(() => {
+		if(socket){
+			console.log("emitting")
+			socket.emit("MESSAGE", {
+				group_id:"test",
+				event_name:"sync",
+				message: syncState,
+				key:"abc123",
+			})
+		}
+	}, [syncState])
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<h1 className="text-5xl font-bold">
+				Welcome to Ignition!
+			</h1>
 			<textarea 
+				defaultValue={tempState}
 				onChange={(e) => setSyncState(e.target.value)}
 				placeholder="Live edit a document with ignition .." 
 				className="p-8 text-xl m-auto w-11/12 h-96 border border-neutral-800 rounded-2xl bg-neutral-900 outline-none"
@@ -28,3 +59,63 @@ export default function Home() {
 		</main>
 	);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client";
+// import { Ignition } from "ignition-js-sdk";
+// import { useEffect, useState } from "react";
+
+// export default function Home() {
+
+// 	const i = new Ignition({
+// 		key:"abc123",
+// 		url:"ws://ignition-shared-v3.onrender.com"
+// 	})
+
+// 	useEffect(() => {
+// 		i.subscribe("test")
+// 		i.on("test", (data) => {
+// 			console.log(data)
+// 		})
+// 	}, [])
+
+// 	const [syncState, setSyncState] = useState("");
+
+// 	useEffect(() => {
+// 		i.emit("test", "test", syncState)
+// 	}, [syncState])
+
+// 	return (
+// 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+// 			<textarea 
+// 				onChange={(e) => setSyncState(e.target.value)}
+// 				placeholder="Live edit a document with ignition .." 
+// 				className="p-8 text-xl m-auto w-11/12 h-96 border border-neutral-800 rounded-2xl bg-neutral-900 outline-none"
+// 			/>
+// 		</main>
+// 	);
+// }
