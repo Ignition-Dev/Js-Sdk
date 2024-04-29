c6595fe795c8a5fd95cd0478c1ad1d1ab4f72b43
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-04-29 22:00:00 +0530

    test: ignition sdk externally

--- playground/src/app/Ignition.js
+++ playground/src/app/Ignition.js
@@ -1,15 +1,27 @@
 import { io } from "socket.io-client";
 import chalk from "chalk";
+import CryptoJS from 'crypto-js';
+
+const { AES } = CryptoJS;
+
+function devLog(...args) {
+	if (process.env.ENV === "dev") console.log(chalk.bold(...args));
+	else return
+}
+
+function errorLog(...args) {
+	console.error(chalk.redBright("Ignition error:", ...args))
+}
 
 export class Ignition {
 	#socket; #encryptionKey;
-    constructor(config) {
+	constructor(config) {
 		this.url = config.url;
 		this.#encryptionKey = config.encryptionKey;
 		this.apiKey = config.key;
 		this.groupId = undefined;
 		this.groupCount = 0;
-		this.#socket = io(this.url, {
+		this.#socket = io(this.url, { // public shared ignition websocket server URL - Elastic Ip
 			auth: {
 				token: this.apiKey,
 			}
@@ -18,26 +30,119 @@ export class Ignition {
 		this.#socket.on("CONNECTED", (message) => { devLog(chalk.cyanBright(message)) });
 	}
 
-    subscribe(groupId) {
-		this.groupId = groupId;
-		this.#socket.emit("JOIN", { key:this.apiKey, group_id:groupId }, (data) => {
-            console.log(data)
-        })
+	ecrypt(message) {
+		return AES.encrypt(message, this.#encryptionKey).toString();
 	}
 
-    on(eventName, callback) {
+	decrypt(message) {
+		return AES.decrypt(message, this.#encryptionKey).toString(CryptoJS.enc.Utf8);
+	}
+
+	async subscribe(groupId) {
+		this.groupId = groupId;
+		devLog("Attempting to subscribe to room !!");
+		this.#socket.emit("JOIN", { key: this.apiKey, group_id: this.groupId }, (data) => { // this would be recived by the server & the server will join this client int that room
+			console.log(chalk.cyanBright(data));
+		});
+	}
+
+	async unsubscribe(groupId) {
+		this.groupId = groupId; // set groupId as global class state
+		devLog("Attempting to unsubscribe from room !!");
+		this.#socket.emit("LEAVE", `${this.apiKey}_${groupId}`, (data) => {
+			console.log(chalk.cyanBright(data));
+		});
+	}
+
+	async emit(eventName, message, group_id=undefined) {
+		if(group_id == undefined && this.groupId == undefined){
+			errorLog("Missing `groupId`. Did you forgot to `subsribe` to a group ?");
+		}
+
+		devLog("EMITTING EVENT !!")
+
+		this.#socket.emit("MESSAGE", {
+			group_id: group_id ? group_id : this.groupId,
+			event_name: eventName,
+			message: typeof(message) == "object" ? JSON.stringify(message) : message,
+			key: this.apiKey
+		})
+	}
+
+	async on(eventName, callback) {
+		if (eventName != "connect" && eventName != "disconnect" && this.groupId == undefined) {
+			errorLog("Missing `groupId`. Did you forgot to `subsribe` to a group ?");
+		};
 		this.#socket.on(eventName, (...data) => {
 			callback(...data)
 		})
 	}
 
-    emit(eventName, message, group_id=undefined) {
-        console.log("emitting event:", eventName, "to group:", this.groupId, "message:", message)
-		this.#socket.emit("MESSAGE", {
-            group_id:this.groupId ? this.groupId : group_id,
-            event_name:eventName,
-            message: typeof(message) == "object" ? JSON.stringify(message) : message,
-            key:this.apiKey,
-        });
+	async off(eventName, callback = undefined) {
+		if (eventName != "connect" && eventName != "disconnect" && this.groupId == undefined) {
+			errorLog("Missing `groupId`. Did you forgot to `subsribe` to a group ?");
+		};
+		this.#socket.off(eventName, callback);
 	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { io } from "socket.io-client";
+// import chalk from "chalk";
+
+// export class Ignition {
+// 	#socket; #encryptionKey;
+//     constructor(config) {
+// 		this.url = config.url;
+// 		this.#encryptionKey = config.encryptionKey;
+// 		this.apiKey = config.key;
+// 		this.groupId = undefined;
+// 		this.groupCount = 0;
+// 		this.#socket = io(this.url, {
+// 			auth: {
+// 				token: this.apiKey,
+// 			}
+// 		});
+// 		this.#socket.on("ERROR", (message) => { errorLog(message) });
+// 		this.#socket.on("CONNECTED", (message) => { devLog(chalk.cyanBright(message)) });
+// 	}
+
+//     subscribe(groupId) {
+// 		this.groupId = groupId;
+// 		this.#socket.emit("JOIN", { key:this.apiKey, group_id:groupId }, (data) => {
+//             console.log(data)
+//         })
+// 	}
+
+//     on(eventName, callback) {
+// 		this.#socket.on(eventName, (...data) => {
+// 			callback(...data)
+// 		})
+// 	}
+
+//     emit(eventName, message, group_id=undefined) {
+//         console.log("emitting event:", eventName, "to group:", this.groupId, "message:", message)
+// 		this.#socket.emit("MESSAGE", {
+//             group_id:this.groupId ? this.groupId : group_id,
+//             event_name:eventName,
+//             message: typeof(message) == "object" ? JSON.stringify(message) : message,
+//             key:this.apiKey,
+//         });
+// 	}
+// }
